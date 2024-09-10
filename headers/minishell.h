84e652049da925a3725b61c3e8976e54fa1cc6a5
ZekaEu <zeka.surf@hotmail.com>
2024-09-10 19:20:56 -0300

    feat: add minishel.h and main.c files

---
+++ headers/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joscarlo <joscarlo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 19:16:33 by joscarlo          #+#    #+#             */
+/*   Updated: 2024/09/10 19:17:331 by joscarlo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <signal.h>
+
+#endif
