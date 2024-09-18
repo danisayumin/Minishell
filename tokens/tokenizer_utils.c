30848bd0e511910adb05fcc846542bfcf2447a1d
Daniela Sayumi <87085378+danisayumin@users.noreply.github.com>
2024-09-18 00:19:16 -0300

    tokenizer aceitando aspas simples e duplas

--- tokens/tokenizer_utils.c
+++ tokens/tokenizer_utils.c
@@ -6,14 +6,53 @@
 /*   By: danielasayuminitta <danielasayuminitta@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 22:58:51 by danielasayu       #+#    #+#             */
-/*   Updated: 2024/09/18 00:12:20 by danielasayu      ###   ########.fr       */
+/*   Updated: 2024/09/18 00:18:18 by danielasayu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/minishell.h"
 
+int	ft_is_quote(char c)
+{
+	if (c == '\'' || c == '"')
+		return (1);
+	return (0);
+}
+
+int	ft_is_separator(char *s)
+{
+	if (!ft_strncmp(s, "&&", 2) || *s == ' ' || *s == '\t'
+		|| *s == '<' || *s == '>' || *s == '|' || *s == '(' || *s == ')')
+		return (1);
+	return (0);
+}
+
 void	ft_skip_spaces(char **line)
 {
 	while (**line && ft_isspace(**line))
-		(*line)++;
+				(*line)++;
+}
+
+bool	ft_skip_quotes(char *line, size_t *i)
+{
+	char	quote;
+
+	quote = line[*i];
+	if (ft_strchr(line + *i + 1, quote))
+	{
+		(*i)++;
+		while (line[*i] != quote)
+			(*i)++;
+		(*i)++;
+		return (true);
+	}
+	return (false);
+}
+
+void	ft_print_quote_err(char c)
+{
+	ft_putstr_fd("minishell: unexpected EOF while looking for matching `", 2);
+	ft_putchar_fd(c, 2);
+	ft_putstr_fd("'\n", 2);
+	g_minishell.exit_s = 258;
 }
