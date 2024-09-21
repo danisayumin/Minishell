922dfd8f9c06a2c2708c306e7999d5535c345d40
ZekaEu <zeka.surf@hotmail.com>
2024-09-21 19:22:01 -0300

    feat: add redirect cases for in, out and append

--- exec/exec.c
+++ exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: joscarlo <joscarlo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 17:02:51 by joscarlo          #+#    #+#             */
-/*   Updated: 2024/09/21 17:23:09 by joscarlo         ###   ########.fr       */
+/*   Updated: 2024/09/21 19:11:22 by joscarlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,4 +91,28 @@ int	ft_exec_node(t_node *tree, bool piped)
 	else
 		return (ft_exec_simple_cmd(tree, piped));
 	return (ENO_GENERAL);
+}
+
+int	ft_err_msg(t_err err)
+{
+	if (err.msg == ERRMSG_CMD_NOT_FOUND)
+		return (ft_putstr_fd("minishell: ", 2), ft_putstr_fd(err.cause, 2),
+			ft_putstr_fd(": command not found\n", 2), err.no);
+	else if (err.msg == ERRMSG_NO_SUCH_FILE)
+		return (ft_putstr_fd("minishell: ", 2), ft_putstr_fd(err.cause, 2),
+			ft_putstr_fd(": No such file or directory\n", 2), err.no);
+	else if (err.msg == ERRMSG_PERM_DENIED)
+		return (ft_putstr_fd("minishell: ", 2), ft_putstr_fd(err.cause, 2),
+			ft_putstr_fd(": Permission denied\n", 2), err.no);
+	else if (err.msg == ERRMSG_AMBIGUOUS)
+		return (ft_putstr_fd("minishell: ", 2), ft_putstr_fd(err.cause, 2),
+			ft_putstr_fd(": ambiguous redirect\n", 2), err.no);
+	else if (err.msg == ERRMSG_TOO_MANY_ARGS)
+		return (ft_putstr_fd("minishell: exit: too many arguments\n", 2),
+			err.no);
+	else if (err.msg == ERRMSG_NUMERIC_REQUI)
+		return (ft_putstr_fd("minishell: exit: ", 2),
+			ft_putstr_fd(err.cause, 2),
+			ft_putstr_fd(": numeric argument required\n", 2), err.no);
+	return (0);
 }
