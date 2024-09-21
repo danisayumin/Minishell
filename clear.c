ec57dbf2f97aecc538413ef1872f44867c2d03af
ZekaEu <zeka.surf@hotmail.com>
2024-09-21 19:07:16 -0300

    feat: add ft_exec_simple_cmd.c file / tbd: ft_get_path

--- clear.c
+++ clear.c
@@ -6,7 +6,7 @@
 /*   By: joscarlo <joscarlo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 20:30:28 by joscarlo          #+#    #+#             */
-/*   Updated: 2024/09/21 17:27:12 by joscarlo         ###   ########.fr       */
+/*   Updated: 2024/09/21 17:27:39 by joscarlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,88 @@ void ft_clean_ms(void)
 	ft_clear_envlst();
 	rl_clear_history();
 	tcsetattr(STDIN_FILENO, TCSANOW, &g_minishell.original_term);
+}
+void	ft_free_char2(char **tofree)
+{
+	size_t	i;
+
+	if (!tofree)
+		return ;
+	i = 0;
+	while (tofree[i])
+		free(tofree[i++]);
+	free(tofree);
+}
+
+char	*ft_strjoin_with(char const *s1, char const *s2, char c)
+{
+	char	*joined;
+	size_t	total_length;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	if (!c || !ft_strlen(s1) || !ft_strlen(s2))
+		return (ft_strjoin(s1, s2));
+	total_length = ft_strlen(s1) + ft_strlen(s2) + 1 + 1;
+	joined = ft_calloc(total_length, sizeof(char));
+	if (!joined)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	joined[i++] = c;
+	j = 0;
+	while (s2[j])
+		joined[i++] = s2[j++];
+	joined[i] = 0;
+	return (joined);
+}
+
+static void	ft_init_leaf(t_node *node)
+{
+	t_io_node	*io;
+	int			p[2];
+	int			pid;
+
+	if (node->args)
+		node->expanded_args = ft_expand(node->args);
+	io = node->io_list;
+	while (io)
+	{
+		if (io->type == IO_HEREDOC)
+		{
+			pipe(p);
+			g_minishell.signint_child = true;
+			pid = (signal(SIGQUIT, SIG_IGN), fork());
+			if (!pid)
+				ft_heredoc(io, p);
+			if (ft_leave_leaf(p, &pid))
+				return ;
+			io->here_doc = p[0];
+		}
+		else
+			io->expanded_value = ft_expand(io->value);
+		io = io->next;
+	}
+}
+
+void	ft_init_tree(t_node *node)
+{
+	if (!node)
+		return ;
+	if (node->type == N_PIPE
+		|| node->type == N_AND
+		|| node->type == N_OR)
+	{
+		ft_init_tree(node -> left);
+		if (!g_minishell.heredoc_sigint)
+			ft_init_tree(node -> right);
+	}
+	else
+		ft_init_leaf(node);
 }
