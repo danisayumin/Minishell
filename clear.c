232f3bdfb529f6446035aa4ffab5aa130772845a
ZekaEu <zeka.surf@hotmail.com>
2024-09-21 20:15:13 -0300

    feat: add ft_get_path.c file

--- clear.c
+++ clear.c
@@ -6,7 +6,7 @@
 /*   By: joscarlo <joscarlo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 20:30:28 by joscarlo          #+#    #+#             */
-/*   Updated: 2024/09/21 17:27:39 by joscarlo         ###   ########.fr       */
+/*   Updated: 2024/09/21 20:14:52 by joscarlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,35 @@ char	*ft_strjoin_with(char const *s1, char const *s2, char c)
 		joined[i++] = s2[j++];
 	joined[i] = 0;
 	return (joined);
+}
+
+char	*ft_strjoin_with_f(char *s1, char *s2, char c)
+{
+	char	*joined;
+	size_t	total_length;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	if (!c || !ft_strlen(s1) || !ft_strlen(s2))
+		return (ft_strjoin(s1, s2));
+	total_length = ft_strlen(s1) + ft_strlen(s2) + 1 + 1;
+	joined = ft_calloc(total_length, sizeof(char));
+	if (!joined)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	joined[i++] = c;
+	j = 0;
+	while (s2[j])
+		joined[i++] = s2[j++];
+	joined[i] = 0;
+	return (free(s1), free(s2), joined);
 }
 
 static void	ft_init_leaf(t_node *node)
---
+++ exec/ft_get_path.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_path.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joscarlo <joscarlo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 19:42:02 by joscarlo          #+#    #+#             */
+/*   Updated: 2024/09/21 20:13:38 by joscarlo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static t_path ft_get_env_path(char *path, char *cmd)
+{
+	size_t	i;
+	t_err	err;
+	char	*cmd_path;
+	char	**split_path;
+
+	split_path = ft_split(path, ':');
+	i = 0;
+	while (split_path[i])
+	{
+		cmd_path = ft_garbage_collector(ft_strjoin_with_f(ft_strdup(split_path[i]), ft_strdup(cmd), '/'), false);
+		err = ft_check_exec(cmd_path, true);
+		if (err.no == ENO_SUCCESS)
+			return((t_path){(t_err){ENO_SUCCESS, 42, cmd_path}, cmd_path});
+		i++;
+	}
+	ft_free_char2(split_path);
+	return ((t_path){(t_err){ENO_NOT_FOUND, ERRMSG_CMD_NOT_FOUND, cmd}, NULL});
+}
+
+t_path	ft_get_path(char *cmd)
+{
+	char	*value;
+	
+	if (*cmd == '\0')
+		return ((t_path){(t_err){ENO_NOT_FOUND, ERRMSG_CMD_NOT_FOUND, cmd}, NULL});
+	if (ft_strnstr(cmd, "/", ft_strlen(cmd)))
+		return ((t_path){ft_check_exec(cmd, false), cmd});
+	value = ft_get_envlst_val("PATH");
+	if (value)
+		return (ft_get_env_path(value, cmd));
+	return ((t_path){(t_err){ENO_NOT_FOUND, ERRMSG_NO_SUCH_FILE, cmd}, NULL});
+}
